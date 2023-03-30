175ca2a55e89746449f706ac16de26e5f3daadb0
wangyu <916491013@qq.com>
2023-03-30 15:40:58 +0800

    build: update config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 // vite.config.js
+import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+import vue from '@vitejs/plugin-vue'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
 export default defineConfig({
+    plugins: [vue(), cssInjectedByJsPlugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
+            // 正则表达式写法
+            '^/api': {
+                target: 'http://127.0.0.1:6806', // 后端服务实际地址
+                changeOrigin: true //开启代理
+            }
+        }
+    },
     build: {
+        assetsDir: '',
+        outDir: 'notegpt',
+        emptyOutDir: false,
         lib: {
-            entry: resolve(__dirname, 'src', "index.ts"),
+            entry: resolve(__dirname, 'src', "main.ts"),
             formats: ['cjs'],
             fileName: 'main',
         },
@@ -14,7 +37,7 @@ export default defineConfig({
         },
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'src', "index.ts"),
+                main: resolve(__dirname, 'src', "main.ts"),
             },
             output: {
                 name: 'main',
